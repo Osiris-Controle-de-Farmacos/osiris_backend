303e55e2b23939c8feeb078ffcd849035afd4826
Mateus Santos Costa <mat3us811@hotmail.com>
2021-02-02 11:30:09 -0300

    Added routes for medicine

---
+++ src/controllers/medicine.ts
@@ -0,0 +1,125 @@
+import { Request, Response } from 'express';
+import db from '../database/';
+export default class MedicineController {
+  // Retorna todas as medicações
+  async index(req: Request, res: Response) {
+    try {
+      const medicines = await db('medicine')
+        .join('storage', 'medicine.id', 'storage.idMedicine')
+        .select(
+          'medicine.id',
+          'medicine.name',
+          'medicine.description',
+          'storage.amount',
+        );
+      return res.status(200).json(medicines);
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({
+        error: 'Houve um erro ao listar as medicações.'
+      });
+    }
+  }
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+
+    try {
+      const medicine = await db('medicine')
+        .join('storage', 'medicine.id', 'storage.idMedicine')
+        .where('medicine.id', id)
+        .select(
+          'medicine.id',
+          'medicine.name',
+          'medicine.description',
+          'storage.amount',
+        );
+      return res.status(200).json(medicine);
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({
+        error: 'Houve um erro ao listar a medicação.'
+      });
+    }
+  }
+  async create(req: Request, res: Response) {
+    const trxProvider = await db.transactionProvider();
+    const trx = await trxProvider();
+
+    try {
+      const {
+        name,
+        description,
+        initialAmount,
+      } = req.body;
+
+      const medicine = await trx('medicine').insert({ name, description });
+
+      const storage = {
+        amount: initialAmount,
+        idMedicine: medicine[0]
+      }
+
+      await trx('storage').insert(storage);
+
+      await trx.commit();
+      return res.status(201).json({
+        msg : "Medicação cadastrada com sucesso"
+      });
+
+    } catch (err) {
+      await trx.rollback();
+      return res.status(400).json({
+        error: 'Erro ao cadastrar cliente.'
+      });
+    }
+  }
+  async update(req: Request, res: Response) {
+    const trxProvider = await db.transactionProvider();
+    const trx = await trxProvider();
+
+    try {
+      const { id, name,  description, amount } = req.body;
+      
+      const medicine = {
+        id: id,
+        name: name,
+        description: description,
+      }
+      const storage = {
+        amount: amount,
+        idMedicine: id,
+      }
+
+      await trx('medicine').update(medicine).where('id', id);
+      if (amount !== null){
+        await trx('storage').update(storage).where('idMedicine', id);
+      }
+      await trx.commit();
+
+      return res.status(204).send();
+    } catch (error) {
+      console.log(error);
+      await trx.rollback();
+      return res.status(400).json({
+        error: 'Unexpected error while updating client.'
+      });
+    }
+  }  
+  async delete(req: Request, res: Response) {
+    const trxProvider = await db.transactionProvider();
+    const trx = await trxProvider();
+    try {
+      const { id } = req.params;
+      await trx('storage').delete().where('idMedicine',id);
+      await trx('medicine').delete().where('id',id);
+      await trx.commit();
+
+      res.status(204).send();
+    } catch (error) {
+      await trx.rollback();
+      res.status(400).json({
+        error: 'Unexpected error while deleting client phone.'
+      });
+    }
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,25 @@
 import express from 'express';
-import UserControllers from './controllers/user';
 
+// Imports dos controllers
+import UserControllers from './controllers/user';
+import MedicineControllers from './controllers/medicine';
+
+// Estanciamento dos controllers
 const userControllers = new UserControllers();
+const medicineControllers = new MedicineControllers();
+
 const routes = express.Router();
 
+// Criação das rotas para executar cada controller
+
+//Users
 routes.get('/', userControllers.index);
+
+// Medicines
+routes.get('/medicines', medicineControllers.index);
+routes.get('/medicine/:id', medicineControllers.show);
+routes.post('/medicine', medicineControllers.create);
+routes.put('/medicine', medicineControllers.update);
+routes.delete('/medicine/:id', medicineControllers.delete);
 
 export default routes;
