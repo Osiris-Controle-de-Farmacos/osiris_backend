7a7307c8ea18d45dd8a8b6cf53b3c2bb12238925
Guilherme Nascimento <guilhermen2k@gmail.com>
2021-02-25 17:36:21 -0300

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
-import express from 'express';
-import routes from './routes';
-
+import express from "express";
+import routes from "./routes";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
-
-app.use('/api', routes);
-
+app.use(cors());
+app.use("/api", routes);
 app.listen(3333);
